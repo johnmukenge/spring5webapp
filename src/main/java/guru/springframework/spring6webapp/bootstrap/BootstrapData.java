ca037f61ae9f7dbd72119360524740a74ec7066f
MukengeNJ <john.mukenge>
2023-04-06 18:36:25 +0200

    jpa repositories: Assignment 1 - Add Publisher Entity

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,14 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -39,8 +45,16 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setTitle("J2EE Development without EJB");
         ddd.setIsbn("678953");
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("Pipo");
+        publisher.setCity("Roma");
+        publisher.setZip("6$%3242");
+        publisher.setAddress("Via Guglielmo Oberdan");
+        publisher.setState("Italy");
+
         Author johnsonSaved = authorRepository.save(johnson);
         Book noEJBSaved = bookRepository.save(noEJB);
+        Publisher publisherSaved = publisherRepository.save(publisher);
 
         // create association between those things
 
@@ -49,9 +63,11 @@ public class BootstrapData implements CommandLineRunner {
 
         authorRepository.save(johnSaved);
         authorRepository.save(johnsonSaved);
+        publisherRepository.save(publisherSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count " + bookRepository.count());
+        System.out.println("Publisher " + publisherRepository.count());
     }
 }
